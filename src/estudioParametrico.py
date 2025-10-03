c6d84300f9fb9b15c01b4582eda10e6263b1183c
Alfonso-Her <47159592+Alfonso-Her@users.noreply.github.com>
2025-10-03 21:03:41 +0200

    Basicamente he refactorizado la parte de estudioParametrico finalmente he tenido que incrustar el json dentro del html por problemas de permisos

--- src/estudioParametrico.py
+++ src/estudioParametrico.py
@@ -3,13 +3,15 @@ from pathlib import Path
 import numpy as np
 from numpy import ndarray
 from datetime import datetime
+from time import time
+import cv2
 from copy import deepcopy
 from resolutorBase import tuberia_preprocesado, obtener_camino, draw_string_art_svg
 
 parametros_preprocesado = ["ruta_a_la_imagen","numero_de_pines","distancia_minima"]
 parametros_resolucion = ["numero_de_pines","distancia_minima","maximo_lineas","peso_de_linea"]
 parametros_reconstruccion = ["tamano_lado_px","ancho_clavos","ancho_de_hilo","ratio_distancia","color_de_hilo","color_de_fondo","color_de_clavo"]
-parametros_a_guardar_json = ["ruta_a_la_imagen","numero_de_pines","secuencia_pines","distancia_minima","maximo_lineas","peso_de_linea","error_total","tiempo_ejecucion","ruta_resultado"]
+parametros_a_guardar_json = ["imagen_original","numero_de_pines","secuencia_pines","distancia_minima","maximo_lineas","peso_de_linea","error_total","tiempo_ejecucion","ruta_resultado"]
 Ruta_a_web = Path("index.html")
 
 def agregarValor(parametros_fijos,clave,valor):
@@ -81,9 +83,9 @@ def concatenar_sobre_json(ruta: Path, metadatos:dict):
         with ruta.open("r", encoding="utf-8") as f:
             data = json.load(f)
     else:
-        data = {}
+        data = []
     
-    data.update(metadatos)
+    data.append(metadatos)
 
     with ruta.open("w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=4,default=convert)
@@ -110,13 +112,15 @@ def estudioParametrico(output_dir:Path, estudio_web:bool,continuacion_estudio:bo
         output_dir = Path(str(output_dir)+hora_proceso)
 
     output_dir.mkdir( parents= True, exist_ok= True)
-    metadatos= {}
     ruta_json = output_dir.joinpath("datos.json")
+    metadatos = []
+    
     # Conseguimos los parametros ya empaquetados para cada parte del problema
     lista_con_todos_los_parametros = construirParametros(**kwargs)
 
     for paquete_argumentos in lista_con_todos_los_parametros:
-        inicio = datetime.now()
+        inicio = time()
+        metadatos_ejecucion= {}
         hora_proceso = "_"+datetime.now().strftime("%d%m%Y_%H%M%S")
         print("\n Estamos procesando los argumentos:",
               f"\n    para el preprocesado:{paquete_argumentos[0]}",
@@ -129,7 +133,7 @@ def estudioParametrico(output_dir:Path, estudio_web:bool,continuacion_estudio:bo
                 print(f"\n {info_si_saltamos}")
                 continue
         try:
-            nombre_foto = paquete_argumentos[0]["ruta_a_la_imagen"].split("/")[-1].split(".")[0]
+            nombre_foto_con_ext = paquete_argumentos[0]["ruta_a_la_imagen"].split("/")[-1].split(".")
             datos_preprocesados = tuberia_preprocesado(**paquete_argumentos[0])
             print("\n Pasamos con exito el preprocesado")
         except Exception as e:
@@ -138,7 +142,7 @@ def estudioParametrico(output_dir:Path, estudio_web:bool,continuacion_estudio:bo
         try:
             paquete_argumentos[1].update({k:v for k,v in datos_preprocesados.items() if k != "posiciones_pines"})
             datos_solucion_problema = obtener_camino(**paquete_argumentos[1])
-            ruta_fichero = output_dir.joinpath(Path(nombre_foto+hora_proceso+"_procesado.svg"))
+            ruta_fichero = output_dir.joinpath(Path(nombre_foto_con_ext[0]+hora_proceso+"_procesado.svg"))
             datos_solucion_problema.update({"ruta_a_resultado": ruta_fichero })
             print("\n Pasamos con exito el proceso de resolucion")
         except Exception as e:
@@ -153,19 +157,36 @@ def estudioParametrico(output_dir:Path, estudio_web:bool,continuacion_estudio:bo
         except Exception as e:
             print(f"\n Error {e} al pintar la solucion, continuando con el siguiente")
             continue
-        fin = datetime.now()
-        metadatos[str(nombre_foto+hora_proceso)] = {}
-        metadatos[str(nombre_foto+hora_proceso)]["ruta_a_la_imagen"] = datos_preprocesados["ruta_a_la_imagen"]
-        metadatos[str(nombre_foto+hora_proceso)]["numero_de_pines"] = datos_preprocesados["numero_de_pines"]
-        metadatos[str(nombre_foto+hora_proceso)]["secuencia_pines"] = datos_solucion_problema["secuencia_pines"]
-        metadatos[str(nombre_foto+hora_proceso)]["distancia_minima"] = datos_solucion_problema["distancia_minima"]
-        metadatos[str(nombre_foto+hora_proceso)]["maximo_lineas"] = datos_solucion_problema["maximo_lineas"]
-        metadatos[str(nombre_foto+hora_proceso)]["peso_de_linea"] = datos_solucion_problema["peso_de_linea"]
-        metadatos[str(nombre_foto+hora_proceso)]["error_total"] = np.sum(datos_solucion_problema["error_total"])
-        metadatos[str(nombre_foto+hora_proceso)]["tiempo_ejecucion"] = fin - inicio
-        metadatos[str(nombre_foto+hora_proceso)]["ruta_resultado"] = datos_sol_final["ruta_resultado"]
 
-    concatenar_sobre_json(ruta_json,metadatos)
+
+        #Agregramos la imagen original si no existe en la carpeta
+        ruta_imagen_original_en_destino = str(output_dir)+"\\"+".".join(nombre_foto_con_ext)
+        if not Path(ruta_imagen_original_en_destino).exists():
+            img_padre = cv2.imread(datos_preprocesados["ruta_a_la_imagen"])
+            cv2.imwrite(ruta_imagen_original_en_destino,img_padre)
+        fin = time()
+
+        metadatos_ejecucion["imagen_original"] = ".".join(nombre_foto_con_ext)
+        metadatos_ejecucion["numero_de_pines"] = datos_preprocesados["numero_de_pines"]
+        metadatos_ejecucion["secuencia_pines"] = datos_solucion_problema["secuencia_pines"].tolist()
+        metadatos_ejecucion["distancia_minima"] = datos_solucion_problema["distancia_minima"]
+        metadatos_ejecucion["maximo_lineas"] = datos_solucion_problema["maximo_lineas"]
+        metadatos_ejecucion["peso_de_linea"] = datos_solucion_problema["peso_de_linea"]
+        metadatos_ejecucion["error_total"] = float(np.sum(datos_solucion_problema["error_total"]))
+        metadatos_ejecucion["tiempo_ejecucion"] = fin - inicio
+        metadatos_ejecucion["ruta_resultado"] = str(datos_sol_final["ruta_resultado"]).split("\\")[-1]
+        
+        concatenar_sobre_json(ruta_json,metadatos_ejecucion)
+        metadatos.append(metadatos_ejecucion)
+
+    if estudio_web:
+        ruta_web = output_dir.joinpath("index.html")
+        with open("index.html","r", encoding="utf-8") as web_origen:
+            contenido = web_origen.read()
+            contenido = contenido.replace("const data = ['cenicero'];",f"const data = {metadatos} ;")
+        with open(ruta_web, "w", encoding="utf-8") as web_destino:
+            web_destino.write(contenido)
+    
 
 
 ## TESTING
@@ -174,6 +195,6 @@ if __name__ == "__main__":
     # print(construirParametros(ruta_a_la_imagen= "HOLA", numero_de_pines= 25, peso_de_linea= [2,20], ancho_clavos = list(range(2))))
     # estudioParametrico(output_dir=Path("../ejemplos/local/prueba"), estudio_web= False,ruta_a_la_imagen= "HOLA",
     #                     numero_de_pines= 25, peso_de_linea= [2,20], ancho_clavos = list(range(2)))
-    estudioParametrico(output_dir=Path("../ejemplos/local/prueba"), estudio_web= False, continuacion_estudio=False,
-                       ruta_a_la_imagen="../ejemplos/ae300.jpg", numero_de_pines= [30,100,256,370],
-                       peso_de_linea= 20, ancho_clavos = 10)
+    estudioParametrico(output_dir=Path("../ejemplos/local/prueba"), estudio_web= True, continuacion_estudio=False,
+                       ruta_a_la_imagen="../ejemplos/ae300.jpg", numero_de_pines= 256,
+                       peso_de_linea= [20,1000], ancho_clavos = 10)
